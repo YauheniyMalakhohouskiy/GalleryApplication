21d22756336af820c72b07213368bfcbcde369d7
YauheniyMalakhohouskiy <malahovskii.evgenii@mail.ru>
2019-09-16 13:29:01 +0300

    Добавлена Пагинация

--- src/aura/GalleryApplication/GalleryApplicationController.js
+++ src/aura/GalleryApplication/GalleryApplicationController.js
@@ -9,7 +9,16 @@
 
     testik:function (component,event,helper) {
         var target = event.target;
+        var element = component.get("v.paginationList");
         var dataEle = target.getAttribute("data-Index");
-        alert(dataEle);
+        alert(dataEle + element[dataEle].Name);
+    },
+
+    next:function (component,event,helper) {
+        helper.next(component, event);
+    },
+
+    previous:function (component,event,helper) {
+        helper.previous(component, event);
     }
 });
--- src/aura/GalleryApplication/GalleryApplicationHelper.js
+++ src/aura/GalleryApplication/GalleryApplicationHelper.js
@@ -10,7 +10,17 @@
             console.log(state);
             if(state == "SUCCESS"){
                 console.log('a.getReturnValue', a.getReturnValue());
+                var pageSize = component.get("v.pageSize");
                 component.set("v.Documents", a.getReturnValue());
+                component.set("v.totalRecords", component.get("v.Documents").length);
+                component.set("v.startPage",0);
+                component.set("v.endPage",pageSize-1);
+                var paginationList = [];
+                for(var i=0; i< pageSize; i++){
+                    if(component.get("v.Documents").length> i)
+                        paginationList.push(a.getReturnValue()[i]);
+                }
+                component.set('v.paginationList', paginationList);
             } else if(state == "ERROR"){
                 alert('Error in calling server side action');
             }
@@ -28,5 +38,47 @@
         });
         $A.enqueueAction(documents);
         $A.enqueueAction(orgId);
+    },
+    
+    next:function (component,event) {
+        var sObjectList = component.get("v.Documents");
+        var end = component.get("v.endPage");
+        var start = component.get("v.startPage");
+        var pageSize = component.get("v.pageSize");
+        var paginationlist = [];
+        var counter = 0;
+        for(var i=end+1; i<end+pageSize+1; i++){
+            if(sObjectList.length > i){
+                paginationlist.push(sObjectList[i]);
+            }
+            counter ++ ;
+        }
+        start = start + counter;
+        end = end + counter;
+        component.set("v.startPage",start);
+        component.set("v.endPage",end);
+        component.set("v.paginationList", paginationlist);
+    },
+
+    previous:function(component,event) {
+        var sObjectList = component.get("v.Documents");
+        var end = component.get("v.endPage");
+        var start = component.get("v.startPage");
+        var pageSize = component.get("v.pageSize");
+        var paginationlist = [];
+        var counter = 0;
+        for(var i= start-pageSize; i < start ; i++){
+            if(i > -1){
+                paginationlist.push(sObjectList[i]);
+                counter ++;
+            }else{
+                start++;
+            }
+        }
+        start = start - counter;
+        end = end - counter;
+        component.set("v.startPage",start);
+        component.set("v.endPage",end);
+        component.set("v.paginationList", paginationlist);
     }
 });
